5d037c3e27c5688ebed26e69346baadd2cddf134
SWING1993 <10446627@qq.com>
2022-05-18 12:37:06 +0800

    Initial Commit

---
+++ IPASigner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IPASigner
+//
+//  Created by SWING on 2022/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IPASigner/IPASignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPASignerApp.swift
+//  IPASigner
+//
+//  Created by SWING on 2022/5/18.
+//
+
+import SwiftUI
+
+@main
+struct IPASignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
